80b73b2ed82547cadf4c88eb7962528cf1170ff2
Vlad Kovac <currently.unkwn@gmail.com>
2026-03-24 18:49:09 +0200

    feat: Add new report for 2025 year, stats updated

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -80,7 +80,7 @@ export const partnersData = {
 export const achievmentsData = {
   ua: [
     {
-      title: "696",
+      title: "734",
       description: "креативних заходів проведено",
       id: "primary",
       "left-translateX": "-100%",
@@ -88,7 +88,7 @@ export const achievmentsData = {
       "right-rotate": "270deg",
     },
     {
-      title: "1032",
+      title: "1036",
       description: "соціальних велосипедів видано",
       id: "primary-light",
       "left-translateX": "-100%",
@@ -97,7 +97,7 @@ export const achievmentsData = {
       "right-rotate": "180deg",
     },
     {
-      title: "11",
+      title: "13",
       description: "публічних просторів оновлено",
       id: "accent",
       "left-translateX": "-100%",
@@ -108,7 +108,7 @@ export const achievmentsData = {
   ],
   en: [
     {
-      title: "696",
+      title: "734",
       description: "creative events held",
       id: "primary",
       "left-translateX": "-100%",
@@ -116,7 +116,7 @@ export const achievmentsData = {
       "right-rotate": "270deg",
     },
     {
-      title: "1032",
+      title: "1036",
       description: "community bikes distributed",
       id: "primary-light",
       "left-translateX": "-100%",
@@ -125,7 +125,7 @@ export const achievmentsData = {
       "right-rotate": "180deg",
     },
     {
-      title: "11",
+      title: "13",
       description: "public spaces revitalized",
       id: "accent",
       "left-translateX": "-100%",
--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -62,6 +62,7 @@ export function getBreakpoints(prefix, prop) {
     ? {
         [`default:${prefix}-${prop.default}`]: prop.default,
         [`tablet:${prefix}-${prop.tablet}`]: prop.tablet,
+        [`tabletS:${prefix}-${prop.tabletS}`]: prop.tabletS,
         [`laptop:${prefix}-${prop.laptop}`]: prop.laptop,
         [`desktop:${prefix}-${prop.desktop}`]: prop.desktop,
       }
